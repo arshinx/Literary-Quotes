58de5caac5f87e7e12c00880f6f52ee4a5593340
Arshin Jain <arshinx@users.noreply.github.com>
2019-10-29 15:25:17 -0400

    ColorBook

---
+++ app/src/main/java/com/arshin/literaryquotes/ColorBook.java
@@ -0,0 +1,26 @@
+package com.arshin.literaryquotes;
+
+import android.graphics.Color;
+
+import java.util.Random;
+
+public class ColorBook {
+
+    // Properties
+    private String[] colors = {
+            "#39add1", // light blue
+            "#3079ab", // dark blue
+            "#c25975", // mauve
+            "#e15258", // red
+            "#f9845b", // orange
+            "#838cc7", // lavender
+            "#7d669e", // purple
+            "#53bbb4", // aqua
+            "#51b46d", // green
+            "#e0ab18", // mustard
+            "#637a91", // dark gray
+            "#f092b0", // pink
+            "#b7c0c7"  // light gray
+    };
+
+}
