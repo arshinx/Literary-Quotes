0a5854db592e56510657b8e573f3b512536003ba
Arshin Jain <arshinx@users.noreply.github.com>
2019-10-29 15:20:16 -0400

    Extension and getting generic view elements

--- app/src/main/java/com/arshin/literaryquotes/MainActivity.java
+++ app/src/main/java/com/arshin/literaryquotes/MainActivity.java
@@ -11,3 +11,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
+
+
+//class  View  {}
+//class Button extends View {}
+//class TextView  extends View {}
+//
+//class Activity {
+//    <T extends View> T findViewById(int id) {
+//       View view = new View();
+//        if (id  == 1) {
+//            view = new Button();
+//        }
+//        if (id  == 2) {
+//            view = new TextView();
+//        }
+//        return view;
+//    }
+//}
+
+//class MainActivity extends Activity {
+//    MainActivity() {
+//        Button button = findViewById(1);
+//        TextView textView = findViewById(2);
+//    }
+//}
